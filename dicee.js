ab2ad09b825d97cbeb5860e74c93f1b744895765
George Pavlos <120208246+geo14150@users.noreply.github.com>
2024-01-06 23:26:28 +0200

    dicee.js

--- dicee.js
+++ dicee.js
@@ -1,59 +1,45 @@
-//function handleButtonClick() {
-  //  const images = [ 'dice1.png','dice2.png','dice3.png','dice4.png','dice5.png','dice6.png'
-    //];
+var btn = document.querySelector("#mybtn");
 
-    
-  //let randomIndex1 = Math.floor(Math.random() * images.length);
-  //  let randomIndex2 = Math.floor(Math.random() * images.length);
+btn.addEventListener("click", function() {
+  rollDice();
+   
+});
+function rollDice(){
+  var randomNumber1=Math.floor(Math.random()*6)+1;//1-6
+  var randomNumber2=Math.floor(Math.random()*6)+1;
 
-  //  randomImage1 = images[randomIndex1];
-  //  randomImage2 = images[randomIndex2];
+  var randomImage1="dice"+randomNumber1+".png";//dice1.png-dice6.png
+  var randomImage2="dice"+randomNumber2+".png";
 
-  //  const img1Elements = document.getElementsByClassName('img1');
-  //    for (let i = 0; i < img1Elements.length; i++) {
-   //     img1Elements[i].src = randomImage1;
-  //    }
+  var imageSource1= "images/"+ randomImage1;//images/dice1.png-images/dice6.png
+  var imageSource2= "images/"+ randomImage2;
 
-  //    // Set the random image for elements with class 'img2'
-  //    const img2Elements = document.getElementsByClassName('img2');
-  //    for (let i = 0; i < img2Elements.length; i++) {
-  //      img2Elements[i].src = randomImage2;
-  //    }
-//}
+  var image1=document.querySelectorAll("img")[0];//class="img1" in html
+  var image2=document.querySelectorAll("img")[1];//class="img2" 
 
-// document.addEventListener('DOMContentLoaded', function () {
-    // Attach the event listener to the button
-//    const changeImageButton = document.getElementById('btn');
-//    changeImageButton.addEventListener('click',handleButtonClick);
-//  });
-//const imageIndex1= document.querySelector(".img1");
-//const imageIndex2=document.querySelector(".img2");
-const images = [
-  'dice1.png',
-  'dice2.png',
-  'dice3.png',
-  'dice4.png',
-  'dice5.png',
-  'dice6.png'
-];
-let randomNumber1=Math.floor(Math.random() * images.length);
-let randomNumber2=Math.floor(Math.random() * images.length);
-let randomImage1 = images[randomNumber1];
-let randomImage2 = images[randomNumber2];
-
-
-const image1=document.querySelector(".img1");
-image1.setAttribute("src",randomImage1);
-const image2=document.querySelector(".img2");
-image2.setAttribute("src",randomImage2);
-//document.querySelector(".img2").setAttribute("randomImage");
-if (randomImage1<randomImage2){
-  document.querySelector("h1").innerText="player2 win";
-}
-else if(randomImage1>randomImage2){
-  document.querySelector("h1").innerText="Player1 wins";
-}else{
-  document.querySelector("h1").innerText="Draw";
+  image1.setAttribute("src",imageSource1);
+  image2.setAttribute("src",imageSource2);
+  var resultText = "";
+  if (randomNumber1>randomNumber2){
+    resultText='Player 1 Wins';
+   
+  }
+  else if(randomNumber1<randomNumber2){
+    resultText='Player 2 Wins';
+  }
+  else {
+    resultText='It is a Draw';
+  }
+  
+  updateButtonText(resultText);
 }
 
+function updateButtonText(text) {
+  document.querySelector('#mybtn').innerHTML = text;
+
+  // After showing the result, reset the button text after a delay (e.g., 2 seconds)
+  setTimeout(function() {
+    document.querySelector('#mybtn').innerHTML = 'Refresh again';
+  }, 1000);
+}
 
